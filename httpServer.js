c029f39db8fb3c398f457b1bc410e966350664eb
adamicine <36242767+adamicine@users.noreply.github.com>
2020-12-14 23:52:06 +0100

    Update httpServer.js

--- httpServer.js
+++ httpServer.js
@@ -4,4 +4,6 @@ http.createServer(function(req,res){  //creating a server for http with request 
   res.writeHead(200, {'Content-Type':'text/html'}) //type of response
   res.write("Welcome back :) "); //response
   res.end(); //end of response
-}).listen(8080) //port to listen to
+}).listen(8080, function(req,res){
+  console.log("Running...")
+}) //port to listen to
