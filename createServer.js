34ab24e29e096546357f275488de0dd05c3513be
adamicine <36242767+adamicine@users.noreply.github.com>
2020-12-14 22:27:22 +0100

    Create createServer.js

---
+++ createServer.js
@@ -0,0 +1,7 @@
+var http = require('http'); //calling the http module, saving it to http variable
+
+http.createServer(function(req,res){  //creating a server for http with request and response
+  res.writeHead(200, {'Content-Type':'text/html'}) //type of response
+  res.write("Welcome back :) "); //response
+  res.end(); //end of response
+}).listen(8080) //port to listen to
